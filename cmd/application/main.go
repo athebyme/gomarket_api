ec07e778d8d00d6b5c315fb40b5f4e64b0a80b2d
athebyme <bruhov37@mail.ru>
2024-10-21 21:29:39 +0300

    Changes:
    - the way code is running

--- cmd/application/main.go
+++ cmd/application/main.go
@@ -1,38 +1,16 @@
 package main
 
 import (
-	"gomarketplace_api/build/_postgres"
-	"gomarketplace_api/internal/wholesaler/products"
-	"gomarketplace_api/pkg/dbconnect/migration"
-	"gomarketplace_api/pkg/dbconnect/postgres"
+	wsapp "gomarketplace_api/internal/wholesaler/app"
+	wbapp "gomarketplace_api/internal/wildberries/app"
 	"log"
 )
 
 func main() {
-	log.Printf("\nStarted App\n")
+	log.Printf("\nStarted app\n")
+	wserver := wsapp.NewWServer()
+	wserver.Run()
 
-	var db, err = postgres.ConnectToPostgreSQL()
-	if err != nil {
-		log.Printf("Error connecting to PostgreSQL: %s\n", err)
-	}
-	defer db.Close()
-
-	migrationApply := []migration.MigrationInterface{
-		&products.WholesalerSchema{},
-		&products.WholesalerProducts{},
-		&products.WholesalerDescriptions{},
-		&products.WholesalerPrice{},
-		&products.WholesalerStock{},
-
-		&_postgres.CreateWildberriesSchema{},
-		&_postgres.CreateCategoriesTable{},
-		&_postgres.CreateProductsTable{},
-	}
-
-	for _, _migration := range migrationApply {
-		if err := _migration.UpMigration(db); err != nil {
-			log.Fatalf("Migration failed: %v", err)
-		}
-	}
-	log.Println("Migrations applied successfully!")
+	wbserver := wbapp.NewWbServer()
+	wbserver.Run()
 }
